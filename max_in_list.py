f41796df3cc7f280669b5beff7b6a548a4ba55c7
manmohanalla <sendmanmohan@gmail.com>
2016-11-08 09:37:39 -0800

    second commit

---
+++ max_in_list.py
@@ -0,0 +1,17 @@
+'''Using the higher order function reduce(), write a function max_in_list() that 
+takes a list of numbers and returns the largest one.'''
+
+def max_in_list(lst):
+
+	max_numb = reduce((lambda x,y:max(x,y)),lst)
+	return max_numb
+
+
+def maxi(lst):
+
+	maximum = lst[0]
+
+	for i in lst:
+		if i > maximum:
+			maximum=i
+	return maximum
