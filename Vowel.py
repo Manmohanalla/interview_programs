187f086e9694e85f70dd8c3825de085215f692d3
manmohana <sendmanmohan@gmail.com>
2016-11-08 00:03:49 -0800

    first commit

--- Vowel.py
+++ Vowel.py
@@ -2,14 +2,11 @@ def vowel(char):
 	
 	# reads a line from input
 	char = char.lower()
-
 	# Check if a character
 	if len(char)!=1 or not char.isalpha():
 		return 'Only 1 alpha character is allowed'
-
 	# vowels
 	vowels = 'aeiou'
-
 	# condition to check if vowel
 	if char in vowels:
 		return True
--- palindrome.py
+++ palindrome.py
@@ -3,22 +3,18 @@ def palindrome(my_str):
 	
 	#converting string to lower cases
 	my_str = my_str.lower()
-
 	# reverse the string
 	rev_str = reversed(my_str)
-
 	# check if the string is equal to its reverse
 	if list(my_str) == list(rev_str):
 		return True
 	else:
 		return False
 
-
 def palindrome1(my_str):
 
 	#converting string to lower cases
 	my_str = my_str.lower()
-
 	#reading list from the last character and comparing
 	if my_str[::-1] == my_str:
 		return True
